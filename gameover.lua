9df77b25d5bb14244c7957c6a5c4bfa81adc50fe
Ben Kelly <bkelly@uoguelph.ca>
2012-02-12 01:28:56 -0500

    fixed game over screen not always displaying text

--- gameover.lua
+++ gameover.lua
@@ -1,6 +1,7 @@
 love.graphics.setBackgroundColor(0,0,0)
 
 function love.draw()
+    love.graphics.setColor(255, 255, 255)
     love.graphics.printf("GAME OVER", 400, 280, 0, "center")
     love.graphics.printf("Score: "..love.score, 400, 320, 0, "center")
 end
